4d94cfa37387a504024459d235f38b7e0c854692
Sebastian Huff <sebhuff@hotmail.com>
2024-07-08 12:23:14 +0100

    added GraphicsView

--- FastFlightObserver/FastFlightObserver/MainPage.xaml.cs
+++ FastFlightObserver/FastFlightObserver/MainPage.xaml.cs
@@ -9,16 +9,9 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void OnClick(object sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		
 	}
 }
 
